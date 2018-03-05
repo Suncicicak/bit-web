24ff39763355e57c37d758767fec72b8c54cd18b
BIT Student <classroom@bgit.rs>
2018-03-05 17:59:37 +0100

    jQuery with h1

--- jQuery/New folder/script.js
+++ jQuery/New folder/script.js
@@ -1,7 +1,7 @@
 $(function () {
     var unicorns = ['https://img00.deviantart.net/8093/i/2004/137/6/e/black_unicorn.jpg', 'http://images2.fanpop.com/images/photos/5700000/Black-Unicorn-fantasy-animals-5768814-430-343.jpg', 'https://3.bp.blogspot.com/-B5T0SIV26wo/Vr7e-tQOzYI/AAAAAAAAKcg/ehitJzwaviI/s1600/288281.jpg']
 
-    var div = $('<div>Image gallery</div>');
+    var div = $('<div><h1>Image gallery</h1></div>');
     $('body').append(div);
 
     for (var i = 0; i < unicorns.length; i++) {
